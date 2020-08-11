da29d72e0a677094d8957ce1affee3263db657c3
grimmgr <ggrimm33@gmail.com>
2020-08-11 11:02:07 -0700

    questions

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Title'
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description:'
+    }, 
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation:'
+    }, 
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage:'
+    }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License:',
+        choices: [
+          'GNU GPLv3',
+          'GNU GPLv2',
+          'Apache 2.0',
+          'BSD 3-Clause',
+          'BSD 2-Clause',
+          'MIT'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contributing:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Tests:'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Questions:'
+    }
 ];
 
 // function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `#${data.title}
 
-`;
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  Licensed under the ${data.license} license.
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
+
+  `;
 }
 
 module.exports = generateMarkdown;
